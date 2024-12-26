88011561d976bc09b323bea45d9f930d5b44a956
johnbean393 <johnbean393@gmail.com>
2024-12-26 10:59:44 +0800

    Initial commit.

--- Tests/DefaultModelsTests/DefaultModelsTests.swift
+++ Tests/DefaultModelsTests/DefaultModelsTests.swift
@@ -1,6 +1,26 @@
+import Foundation
 import Testing
 @testable import DefaultModels
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+@Test
+func exportModelJson() async throws {
+	// Get list of models
+	let models: [HuggingFaceModel] = DefaultModels.models
+	// Initialize the encoder
+	let encoder: JSONEncoder = JSONEncoder()
+	// Define JSON file URL
+	let jsonUrl: URL = URL(fileURLWithPath: "/Users/bj/Library/Application Support/Magic Sorter/Sorted Land/Computer DN/Xcode Apps/Misc/Sidekick/Models/models.json")
+	// Save models to JSON file
+	do {
+		// Convert models to data
+		let rawData: Data = try encoder.encode(models)
+		// Save to JSON file
+		try rawData.write(
+			to: jsonUrl,
+			options: .atomic
+		)
+	} catch {
+		print("Error encoding data: \(error)")
+	}
+
 }
