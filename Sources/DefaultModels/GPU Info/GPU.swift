134c87124a1adbaf7d0974baad3ab35f6c8d8d36
johnbean393 <johnbean393@gmail.com>
2026-04-04 13:13:46 -0400

    feat: Add A18 Pro, M5 Pro, M5 Max

--- Sources/DefaultModels/GPU Info/GPU.swift
+++ Sources/DefaultModels/GPU Info/GPU.swift
@@ -18,22 +18,25 @@ public struct GPU: Identifiable {
 	public var tflops: Double
 	
 	public static let all: [GPU] = [
-		.init(name: "Apple M1", tflops: 2.617),
-		.init(name: "Apple M1 Pro", tflops: 5.308),
-		.init(name: "Apple M1 Max", tflops: 10.617),
-		.init(name: "Apple M1 Ultra", tflops: 21.23),
-		.init(name: "Apple M2", tflops: 3.579),
+        .init(name: "Apple A18 Pro", tflops: 1.9),
+		.init(name: "Apple M1", tflops: 2.6),
+		.init(name: "Apple M1 Pro", tflops: 5.2),
+		.init(name: "Apple M1 Max", tflops: 10.4),
+		.init(name: "Apple M1 Ultra", tflops: 21.0),
+		.init(name: "Apple M2", tflops: 3.6),
 		.init(name: "Apple M2 Pro", tflops: 6.8),
-		.init(name: "Apple M2 Max", tflops: 13.6),
+		.init(name: "Apple M2 Max", tflops: 13.49),
 		.init(name: "Apple M2 Ultra", tflops: 27.2),
 		.init(name: "Apple M3", tflops: 4.1),
 		.init(name: "Apple M3 Pro", tflops: 7.4),
-		.init(name: "Apple M3 Max", tflops: 16.2),
-        .init(name: "Apple M3 Ultra", tflops: 32.4),
+		.init(name: "Apple M3 Max", tflops: 14.2),
+        .init(name: "Apple M3 Ultra", tflops: 28.4),
         .init(name: "Apple M4", tflops: 4.6),
         .init(name: "Apple M4 Pro", tflops: 9.2),
         .init(name: "Apple M4 Max", tflops: 18.4),
         .init(name: "Apple M5", tflops: 5.7),
+        .init(name: "Apple M5 Pro", tflops: 11.4),
+        .init(name: "Apple M5 Max", tflops: 22.8),
 	].sorted(by: {
 		$0.tflops < $1.tflops
 	})
